dc98a01d53c5361c87edbe01e09cd83a8b4f078e
Hai Truong <truonghoahai@gmail.com>
2022-08-04 22:01:53 +0700

    New

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import PrimeVue from "primevue/config";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App);
+app.use(PrimeVue);
+app.use(store);
+app.use(router);
+
+app.mount("#app");
